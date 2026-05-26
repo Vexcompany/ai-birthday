f69224cd28df80b232ba79d592f1a94de8c414da
Vexcompany <vexcorporation43@gmail.com>
2026-05-26 19:52:38 +0700

    Create checkBirthday.js

---
+++ api/checkBirthday.js
@@ -0,0 +1,28 @@
+const members = require("../members.json");
+
+module.exports = async (req, res) => {
+
+  const now = new Date();
+
+  const jakarta = new Date(
+    now.toLocaleString("en-US", {
+      timeZone: "Asia/Jakarta"
+    })
+  );
+
+  const today =
+    String(jakarta.getMonth() + 1).padStart(2, "0") +
+    "-" +
+    String(jakarta.getDate()).padStart(2, "0");
+
+  const ultah = members.filter(
+    x => x.tanggalLahir === today
+  );
+
+  return res.status(200).json({
+    success: true,
+    today,
+    total: ultah.length,
+    members: ultah
+  });
+};
